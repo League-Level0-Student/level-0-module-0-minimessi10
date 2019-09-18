9ef4fbeee0b68b8baef21d88e50a33ada071e52e
i-lab user <i-labuser@SDPL88534-iMacProL.local>
2019-09-17 17:53:09 -0700

    doing robotgraffity
    robotgraffity

---
+++ src/ROBOTGRAFFITY.java
@@ -0,0 +1,27 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class ROBOTGRAFFITY {
+public static void main(String[] args) {
+	Robot messi=new Robot();
+	messi.penDown();
+	messi.move(200);
+	messi.setSpeed(500);
+	messi.turn(100);
+	messi.move();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
--- src/_01_robot_square/RobotSquare.java
+++ src/_01_robot_square/RobotSquare.java
@@ -12,22 +12,33 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+Robot messi=new Robot();
 
         // 3. Put the robot's pen down
-
+messi.penDown();
 
         // 6. Make the robot move as fast as possible
-
+messi.setSpeed(100);
 
         // 5. Do everything below here 4 times
 
 
         //         2. Move your robot 200 pixels
 
-
         //         4. Turn the robot 90 degrees to the right (90 degrees)
 
 
+
+int  messi2=2;
+ while(messi2<6) {
+messi.turn(90);
+messi.move(200);
+messi2=1+messi2;
+
+
+
+
+
+    }
     }
 }
